e6d289469dc7215784e68a408d5b4120f3031479
Aarif <51032249+aarif-razak@users.noreply.github.com>
2020-07-02 13:21:42 -0400

    Commands Register
    Freedome to make commands

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 const config = require("./config.json");
 const Commando = require('discord.js-commando');
 
+const path = require('path');
+
+
+
 const client = new Commando.Client({
     owner: '288782447704014861'
 });
@@ -9,5 +13,9 @@ client.on("ready", () => {
     console.log("PEEPEE!");
 });
 
+client.registry
+    .registerDefaults()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+    
 
 client.login(config.token);
